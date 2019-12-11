d84cad4db806c3b5f519b1f2c4a1f88aa94a6842
vpbobade <51505151+vpbobade@users.noreply.github.com>
2019-12-11 15:13:27 +0530

    Update make_docker_image.sh

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cp -rf /var/lib/jenkins/workspace/Q_pipeline/Dockerfile /opt
-cp -rf /var/lib/jenkins/workspace/Q_pipeline/webapp/target/*.war /opt
+mkdir -p /opt/docker_image
+cp -rf /var/lib/jenkins/workspace/Q_pipeline/Dockerfile /opt/docker_image
+cp -rf /var/lib/jenkins/workspace/Q_pipeline/webapp/target/*.war /opt/docker_image
 cp -rf /var/lib/jenkins/workspace/Q_pipeline/webapp/target/*.war /var/lib/jenkins/workspace/
 docker build -f Dockerfile  -t vpbobade/mydockerimage:1.0.0 . > docker_out
 image_name=`cat docker_out | tail -1 | awk -F' ' ' { print $3 } '` 
