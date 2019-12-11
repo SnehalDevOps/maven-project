d674823ba3f43fbdf60286c5278d6812e8b97433
root <root@ip-172-31-81-65.ec2.internal>
2019-12-11 09:26:01 +0000

    script_canged

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -3,6 +3,7 @@
 cp -rf /var/lib/jenkins/workspace/Q_pipeline/Dockerfile /root
 cp -rf /var/lib/jenkins/workspace/Q_pipeline/webapp/target/*.war /root
 chmod -R 777 Dockerfile *.war
+chown jenkins:jenkins Dockerfile *.war
 docker build -f Dockerfile  -t vpbobade/mydockerimage:1.0.0 . > docker_out
 image_name=`cat docker_out | tail -1 | awk -F' ' ' { print $3 } '` 
 echo "Image created as $image_name"
