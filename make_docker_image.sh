758cc6e56ebe95c2b4b42c49cb60f2f9767df655
vpbobade <51505151+vpbobade@users.noreply.github.com>
2019-12-11 15:14:58 +0530

    Update make_docker_image.sh

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p /opt/docker_image
-cp -rf /var/lib/jenkins/workspace/Q_pipeline/Dockerfile /opt/docker_image
-cp -rf /var/lib/jenkins/workspace/Q_pipeline/webapp/target/*.war /opt/docker_image
-cp -rf /var/lib/jenkins/workspace/Q_pipeline/webapp/target/*.war /var/lib/jenkins/workspace/
-docker build -f Dockerfile  -t vpbobade/mydockerimage:1.0.0 . > docker_out
+sudo mkdir -p /opt/docker_image
+sudo cp -rf /var/lib/jenkins/workspace/Q_pipeline/Dockerfile /opt/docker_image
+sudo cp -rf /var/lib/jenkins/workspace/Q_pipeline/webapp/target/*.war /opt/docker_image
+sudo cp -rf /var/lib/jenkins/workspace/Q_pipeline/webapp/target/*.war /var/lib/jenkins/workspace/
+sudo docker build -f Dockerfile  -t vpbobade/mydockerimage:1.0.0 . > docker_out
 image_name=`cat docker_out | tail -1 | awk -F' ' ' { print $3 } '` 
 echo "Image created as $image_name"
